4cbd8ea33d022ce9cddcc2d1d0f4188c4628c77f
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2021-01-07 17:44:55 +0900

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
+
+const webpack = require('webpack');
 const path = require('path')
+
+/*module.exports = {
+	entry: './assets/js/app.js',
+	output: {
+		filename: 'script.js',
+		path: path.resolve(__dirname, 'public/js'),
+	},
+}*/
 
 module.exports = {
 	entry: './assets/js/app.js',
@@ -6,4 +16,11 @@ module.exports = {
 		filename: 'script.js',
 		path: path.resolve(__dirname, 'public/js'),
 	},
+	// https://webpack.js.org/plugins/ignore-plugin/
+	plugins: [
+		new webpack.IgnorePlugin({
+			resourceRegExp: /^\.\/locale$/,
+			contextRegExp: /moment$/
+		}),
+	],
 }
