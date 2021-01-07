fad387ed64a0cf19c679b67af169fde465e8c51b
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2021-01-07 16:27:11 +0900

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
 	entry: './assets/js/index.js',
@@ -6,4 +6,4 @@ module.exports = {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'public/js'),
 	},
-};
+}
